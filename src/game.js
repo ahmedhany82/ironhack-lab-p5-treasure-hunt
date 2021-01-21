c99b4c592b2c3d5b1c129f786d346ed50f12067a
Ahmed Abdelsalam <ahmedabdelsalam82.de@gmail.com>
2021-01-21 18:52:11 +0100

    done with iterations 1-5

--- src/game.js
+++ src/game.js
@@ -1,7 +1,96 @@
 class Game {
+
+  constructor() {
+    this.player = new Player(0, 0);
+    this.treasure = new Treasure(500, 500);
+  }
+
+  preload() {
+    this.player.playerPreload();
+    this.treasure.treasurePreload();
+  }
+
   drawGrid() {
     // Iteration 1
     // Draw the grid
     // https://p5js.org/reference/#/p5/line
+
+    clear();
+    stroke(0);
+    for(let i= 0; i<=1000; i+=100)
+    {
+      line(0, i, WIDTH, i); 
+      line(i, 0, i, HEIGHT);     
+    }
+    this.player.draw();
+  }
+
+  drawGame() {
+    game.player.draw();
+    game.treasure.draw();
+    if(game.player.col === game.treasure.col && game.player.row === game.treasure.row)
+    {
+      game.treasure.setRandomPosition();
+    }  
   }
 }
+
+class Player {
+  constructor(col, row) {
+    this.col = col;
+    this.row = row;
+    this.image;
+  }
+
+  moveUp() {
+    this.row -= 100;
+  }
+
+  moveDown() {
+    this.row += 100; 
+  }
+
+  moveLeft() {
+    this.col -= 100;
+  }
+
+  moveRight() {
+    this.col += 100;
+  }
+
+  playerPreload() {
+    this.image = loadImage('assets/character-down.png');
+  }
+
+  draw() {
+    this.row = constrain(this.row, 0, (HEIGHT - 100))
+    this.col = constrain(this.col, 0, (WIDTH - 100))
+    image(this.image, this.col, this.row, 100, 100)
+  }
+}
+
+class Treasure {
+  constructor(col, row) {
+    this.row = row;
+    this.col = col;
+    this.image;
+  }
+
+  treasurePreload() {
+    console.log('treasuePreload called');
+    this.image = loadImage('assets/treasure.png');
+  }
+
+  setRandomPosition() {
+    console.log("setRandomPosition is called");
+    this.row = (Math.floor(Math.random() * 10)) * 100;
+    this.col = (Math.floor(Math.random() * 10)) * 100;
+    console.log(this.row);
+    console.log(this.col);
+  }
+
+  draw() {
+    image(this.image, this.col, this.row, 100, 100)
+  }
+}
+
--- src/main.js
+++ src/main.js
@@ -7,4 +7,30 @@ function setup() {
 
 function draw() {
   game.drawGrid();
+  // game.player.draw();
+  // game.treasure.draw();
+  game.drawGame();
+}
+
+function preload() {
+  //img = loadImage('assets/character-down.png');
+  //game.player.preload();
+  game.preload();
+}
+
+function keyPressed() {
+  console.log(keyCode);
+  if (keyCode === 38) {
+      game.player.moveUp();
+  }
+  if (keyCode === 40) {
+      game.player.moveDown();
+  }
+  if (keyCode === 37) {
+      game.player.moveLeft();
+  }
+  if (keyCode === 39) {
+      game.player.moveRight();
+  }
+
 }
